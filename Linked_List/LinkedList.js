28cdff7ce239199dcc64ab819edc0ad6c3cd31fb
Andrew Le <s3820098@rmit.edu.vn>
2021-09-28 13:10:44 +0700

    Create LinkedList.js + add prepend method

---
+++ Linked_List/LinkedList.js
@@ -0,0 +1,21 @@
+import LinkedListNode from "./LinkedListNode";
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // add at the beginning
+  prepend(newValue) {
+    // set new Node as Head
+    const newNode = new LinkedListNode(newValue);
+    this.head = newNode;
+
+    // if there is no tail, set new Node as Tail also
+    if (!this.tail) this.tail = newNode;
+    return this;
+  }
+    
+  
+}
--- Linked_List/LinkedListNode.js
+++ Linked_List/LinkedListNode.js
@@ -1,10 +1,9 @@
 export default class LinkedListNode {
+    /**
+     * @param {containing value, link to next node} 
+    */
   constructor(value, next = null) {
     this.value = value;
     this.next = next;
-  }
-
-  toString(callback) {
-    return callback ? callback(this.value) : `${this.value}`;
   }
 }
